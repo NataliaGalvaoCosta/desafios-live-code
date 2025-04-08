63ebd82cf4b3c97a4891dbe0627fb63754293055
NataliaGalvaoCosta <natygalvaocosta@gmail.com>
2025-04-08 18:12:32 -0300

    Criando os métodos multiplicar e subtrair do live_code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int somar = soma(3, 7);
+
+        int dividir = divide(somar);
+
+        int multiplicar = multiplica(dividir);
+
+        int subtrair = subtrai(multiplicar);
+
+    }
+    public static int soma(int a,int b) {
+     int total = a+b;
+
+        System.out.println("O resultado da soma é " + total);
+        return total;
+    }
+    public static int divide(int soma){
+        int total2 = soma/2;
+
+        System.out.println("O resultado da divisão é " + total2);
+        return total2;
+    }
+
+    public static int multiplica(int divide){
+        int total3 = divide*10;
+
+        System.out.println("O resultado da multiplicação é " + total3);
+        return total3;
+    }
+
+    public static int subtrai(int multiplica){
+        int total4 = multiplica-100;
+
+        System.out.println("O resultado da subtração é " + total4);
+        return total4;
+    }
+
+}
